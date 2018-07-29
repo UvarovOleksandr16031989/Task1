fc269eb699a700cee24e10c9e291f434a9681c0d
UvarovOleksandr16031989 <alexuvarov1989@ukr.net>
2018-07-29 15:39:04 +0300

    bagFix

--- js/script.js
+++ js/script.js
@@ -39,9 +39,25 @@ while(x<2019){
     </option>`;
     x++;
 }
- $('#map_btn').click(function(){
-     $('.map_clients').toggleClass('is-hidden');
-     $('html, body').animate({
-         scrollTop: $("#map").offset().top
-     }, 500);
- });
+var $win = $(window),
+    $fixed = $(".button_top"),
+    limit = 300;
+
+function tgl (state) {
+    $fixed.toggleClass("hidden", state);
+}
+
+$win.on("scroll", function () {
+    var top = $win.scrollTop();
+
+    if (top < limit) {
+        tgl(true);
+    } else {
+        tgl(false);
+    }
+});
+
+$('#btn_head').click(function(){
+    $('.section_header__burger_item').toggleClass('is-active');
+    $('.section_header__menu').toggleClass('hidden');
+});
