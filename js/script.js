17b345a5eaa149c619303e0338a1abc6ee41d53c
UvarovOleksandr16031989 <alexuvarov1989@ukr.net>
2018-07-31 18:38:45 +0300

    Update 2018-07-31T15:38:35.003Z

--- js/script.js
+++ js/script.js
@@ -1,63 +1,43 @@
-var elem= document.getElementById('DaySelect');
+var elem = document.getElementById('DaySelect');
 var i = 1;
-while(i < 32){
-    elem.innerHTML +=
-    `<option class="form_brithday__day_item">
-        ${i}
-    </option>`;
+while(i < 32) {
+    elem.innerHTML += `<option class="form_brithday__day_item">
+            ${i}
+        </option>`;
     i++;
 }
-var mouth=[
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-var elem=document.getElementById("MonthSelect");
-var n=0;
-while(n<12){
-    elem.innerHTML+=
-    `<option class="form_brithday__month_item">
-        ${mouth[n]}
-    </option>`;
+var mouth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var elem = document.getElementById("MonthSelect");
+var n = 0;
+while(n < 12) {
+    elem.innerHTML += `<option class="form_brithday__month_item">
+            ${mouth[n]}
+        </option>`;
     n++;
 }
-var elem= document.getElementById("yearSelect");
-var x=1920;
-while(x<2019){
-    elem.innerHTML+=
-    `<option class="form_brithday__year_item">
-        ${x}
-    </option>`;
+var elem = document.getElementById("yearSelect");
+var x = 1920;
+while(x < 2019) {
+    elem.innerHTML += `<option class="form_brithday__year_item">
+            ${x}
+        </option>`;
     x++;
 }
-var $win = $(window),
-    $fixed = $(".button_top"),
-    limit = 400;
-
-function tgl (state) {
+var $win = $(window)
+    , $fixed = $(".button_top")
+    , limit = 400;
+function tgl(state) {
     $fixed.toggleClass("hidden", state);
 }
-
-$win.on("scroll", function () {
+$win.on("scroll", function() {
     var top = $win.scrollTop();
-
-    if (top > limit) {
+    if(top > limit) {
         tgl(true);
     } else {
         tgl(false);
     }
 });
-
-$('#btn_head').click(function(){
+$('#btn_head').click(function() {
     $('.section_header__burger_item').toggleClass('is-active');
     $('.section_header__menu').toggleClass('is-hidden');
 });
